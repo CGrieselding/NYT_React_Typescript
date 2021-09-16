3e66b985a105e3b041446387c31ae56441ba7ab2
CGrieselding <camerongrieselding@gmail.com>
2021-09-16 15:46:34 -0400

    NYT app components

---
+++ src/components/NYTApp.tsx
@@ -0,0 +1,134 @@
+// Class Component
+import React from "react";
+import NYTDisplay from "./NYTDisplay";
+
+const baseURL: string =
+  "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+const key: string = "SCZrRgHl8UAUWFVKK0X6OKogZvvfjure";
+
+// type Result = {
+//   abstract: string;
+//   byline: string;
+//   document_type: string;
+//   headline: string;
+//   keywords: string[];
+//   length: number;
+//   lead_paragraph: string;
+//   multimedia: string[];
+//   news_desk: string;
+//   pub_date: string;
+//   section_name: string;
+//   snippet: string;
+//   source: string;
+//   type_of_material: string;
+//   uri: string;
+//   web_url: string;
+//   word_count: number;
+//   _id: string;
+// };
+
+type StateType = {
+  search: string;
+  startDate: string;
+  endDate: string;
+  pageNumber: number;
+  results: string[];
+};
+
+export default class NYTApp extends React.Component<{}, StateType> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      search: "",
+      startDate: "",
+      endDate: "",
+      pageNumber: 0,
+      results: [],
+    };
+  }
+  componentDidMount() {
+    let url: string = `${baseURL}?api-key=${key}&page=${this.state.pageNumber}&q=${this.state.search}`;
+    url = this.state.startDate
+      ? url + `&begin_date=${this.state.startDate}`
+      : url;
+    url = this.state.endDate ? url + `&end_date=${this.state.endDate}` : url;
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => this.setState({ results: data.response.docs }))
+      .catch((err) => console.log(err));
+  }
+
+  componentDidUpdate() {
+    console.log(this.state.results);
+  }
+
+  handleSubmit = (e: any) => {
+    this.setState({ pageNumber: 0 });
+    e.preventDefault();
+    this.componentDidMount();
+  };
+
+  changePageNumber = (e: any, direction: string): any => {
+    e.preventDefault();
+    if (direction === "down") {
+      if (this.state.pageNumber > 0) {
+        this.setState({ pageNumber: this.state.pageNumber - 1 });
+        this.componentDidMount();
+      }
+    }
+    if (direction === "up") {
+      this.setState({ pageNumber: this.state.pageNumber + 1 });
+      this.componentDidMount();
+    }
+  };
+
+  render() {
+    // const articles = {
+    //   results: this.state.results,
+    // };
+
+    // const changePage = {
+    //    pageNumber: this.changePageNumber()
+    // }
+
+    return (
+      <div>
+        <h1 className="mainTitle">New York Times Articles</h1>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <span>Enter a single search term: </span>
+          <input
+            type="text"
+            name="search"
+            onChange={(e) => this.setState({ search: e.target.value })}
+            required
+          />
+          <br />
+          <span>Enter a start date: </span>
+          <input
+            type="date"
+            name="startDate"
+            pattern="[0-9]{8}"
+            onChange={(e) => this.setState({ startDate: e.target.value })}
+          />
+          <br />
+          <span>Enter an end date: </span>
+          <input
+            type="date"
+            name="endDate"
+            pattern="[0-9]{8}"
+            onChange={(e) => this.setState({ endDate: e.target.value })}
+          />
+          <br />
+          <button className="submit">Submit Search</button>
+        </form>
+        {this.state.results.length > 0 ? (
+          <NYTDisplay
+            results={this.state.results}
+            changePage={this.changePageNumber}
+          />
+        ) : null}
+      </div>
+    );
+  }
+}
---
+++ src/components/NYTDisplay.tsx
@@ -0,0 +1,48 @@
+// Functional Component
+import React from "react";
+
+const NYTDisplay = (props: any) => {
+  const { results, changePage } = props;
+
+  return (
+    <div>
+      <div>
+        <button onClick={(e) => changePage(e, "down")}>
+          Previous 10 Articles
+        </button>
+        <button onClick={(e) => changePage(e, "up")}>Next 10 Articles</button>
+      </div>
+      {results.map((result: any) => {
+        return (
+          <div key={result._id}>
+            <h3 className="articleTitle">{result.headline.main}</h3>
+            {result.multimedia.length > 1 ? (
+              <img
+                alt="article"
+                src={`http://www.nytimes.com/${result.multimedia[1].url}`}
+              />
+            ) : (
+              ""
+            )}
+            <p>
+              {result.snippet}
+              <br />
+              {result.keywords.length > 0 ? <p className="keywords">Keywords:</p> : ""}
+            </p>
+            <ul style={{listStyleType: "none"}}>
+              {result.keywords.map((keyword: any) => (
+                <li key={keyword.value}>{keyword.value}</li>
+              ))}
+            </ul>
+            <a href={result.web_url} target="_blank">
+              <button>Read It!</button>
+            </a>
+            <hr />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default NYTDisplay;
